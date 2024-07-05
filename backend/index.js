d47596c371846bbf97b3a29f374712528fddf6c8
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-07-05 18:41:57 +0700

    cmn pakai localhost:5000 bisa jalan

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import friendshipRoutes from './routes/friendship.routes.js';
@@ -9,6 +10,8 @@ import groupMessageRoutes from './routes/groupMessage.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import { app, server } from './socket/socket.js';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -22,12 +25,18 @@ app.use('/api/users', userRoutes); // middleware
 app.use('/api/groups', groupRoutes); // middleware
 app.use('/api/group-messages', groupMessageRoutes); // middleware
 app.use('/api/friend-list', friendshipRoutes); // middleware
+app.use(express.static(path.join(__dirname, '/frontend/dist'))); // middleware
 
-app.get('/', (req, res) => {
-  // root route http://localhost:5000/
-  res.send('Hai World');
+// app.get('/', (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send('Hai World');
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 server.listen(PORT, () => {
   connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
 });
