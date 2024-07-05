bee814d4502734152a0dd87f44d215b89c016971
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-07-05 17:24:00 +0700

    gatau dah

--- frontend/src/components/Sidebar/RequestFriend/RequestFriend.jsx
+++ frontend/src/components/Sidebar/RequestFriend/RequestFriend.jsx
@@ -18,20 +18,12 @@ const RequestFriend = ({ user }) => {
       <div className={`flex gap-2 items-center p-2 rounded-md cursor-pointer hover:bg-slate-100 border-b-2`}>
         <div className={`avatar`}>
           <div className="w-10 rounded-full">
-<<<<<<< HEAD
-            <img alt="user avatars" src="https://avatar.iran.liara.run/public/boy?username=panji" />
-=======
             <img alt="user avatars" src={user.profilePicture} />
->>>>>>> 66d81a7d1ffb6bfe8379f9644e22ef3460dd7649
           </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-<<<<<<< HEAD
-            <p className="font-bold text-xs text text-slate-800">Panji</p>
-=======
             <p className="font-bold text-xs text text-slate-800">{user.fullName}</p>
->>>>>>> 66d81a7d1ffb6bfe8379f9644e22ef3460dd7649
             <div className="flex gap-2">
               <button className="btn bg-success" onClick={handleAccept}>
                 <i className="fa-solid fa-check"></i>
